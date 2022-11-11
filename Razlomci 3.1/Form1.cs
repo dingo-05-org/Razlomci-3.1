b77632ded5592b84672bbd450e4ac9d8f2e75fa7
dingo-05 <112895914+dingo-05@users.noreply.github.com>
2022-11-11 23:00:03 +0100

    Add project files.

---
+++ Razlomci 3.1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Razlomci_3._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Razlomci uBroj (ref int i, string s)
+        {
+            int p = i;
+            while (i < s.Length && ((s[i] >= '0' && s[i] <= '9') || s[i] == '/'))
+                i++;
+            Razlomci rez= new Razlomci(s.Substring(p, i - p));
+            listBox1.Items.Add(rez.ispis());
+            return rez;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s=textBox1.Text;
+            int i = 0;
+            Razlomci rez= uBroj(ref i, s);
+            while(i<s.Length)
+            {
+                char c = s[i];
+                i++;
+                if (c == '+')
+                    rez = rez.saberi(uBroj(ref i, s));
+                else
+                    rez = rez.oduzmi(uBroj(ref i, s));
+
+            }
+            label1.Text = rez.ispis();
+        }
+    }
+}
---
+++ Razlomci 3.1/Razlomci.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razlomci_3._1
+{
+    public class Razlomci
+    {
+
+        int br, im; // atributi brojilac i imenilac 
+
+        #region konstruktori
+        public Razlomci() //podrazumevani konstruktor 
+        {
+            br = 0;
+            im = 1;
+        }
+        public Razlomci(int a)
+        {
+            br = a;
+            im = 1;
+        }
+        public Razlomci(int a, int b) 
+        {
+            br = a; 
+            im = b; 
+            if (im == 0) im = 1; 
+            else if (im < 0) { br = -br; im = -im; } 
+            skrati(); 
+        }
+        public Razlomci(string s)
+        {
+            int poz = s.IndexOf('/');
+
+            if (poz != -1)
+            {
+                br = int.Parse(s.Substring(0, poz));
+                im = int.Parse(s.Substring(poz + 1));
+                skrati();
+            }
+            else
+            {
+                br = int.Parse(s);
+                im = 1;
+            }
+        }
+        #endregion
+        #region metode za skracivanje i ispis razlomka
+        public void skrati()
+        {
+            int d = 2;
+            while (Math.Abs(br) >= d && Math.Abs(im) >= d)
+            {
+                if (br % d == 0 && im % d == 0)
+                {
+                    br /= d;
+                    im /= d;
+                }
+                else
+                    d++;
+            }
+        }
+        public void skrati2()
+        {
+            int a, b, pom;
+            a = Math.Abs(br);
+            b = Math.Abs(im);
+            while (b != 0)
+            {
+                pom = b;
+                b = a % b;
+                a = pom;
+            }
+            br /= a;// a=nzd(a,b) 
+            im /= a;
+        }
+        public string ispis()
+        {
+            if (br == 0) return "0";
+            if (im == 1) return br + "";
+            if (br > 0) return br + "/" + im;
+            else return "(" + br + "/" + im + ")";
+        }
+        #endregion
+        #region operacije sa razlomcima
+        public Razlomci reciprocni()
+        {
+            if (br > 0) return new Razlomci(im, br);
+            else return new Razlomci(-im, -br);
+        }
+        public Razlomci saberi(Razlomci B)
+        { return new Razlomci(this.br * B.im + this.im * B.br, this.im * B.im); }
+        public Razlomci oduzmi(Razlomci B)
+        { return new Razlomci(this.br * B.im - this.im * B.br, this.im * B.im); }
+        public Razlomci pomnozi(Razlomci B)
+        { return new Razlomci(this.br * B.br, this.im * B.im); }
+        public Razlomci podeli(Razlomci B)
+        {
+            return this.pomnozi(B.reciprocni());
+        }
+        #endregion
+    } 
+}
